200a1a7d847542f1080388d91739821c963cf141
Manish Mathai <manishpmathai@gmail.com>
2020-06-02 21:58:02 -0700

    Outputs gradient PPM image
    This finishes Chapter 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+const IMAGE_WIDTH: i32 = 256;
+const IMAGE_HEIGHT: i32 = 256;
+
 fn main() {
-    println!("Hello, world!");
+    println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
+    for j in (0..IMAGE_HEIGHT).rev() {
+        for i in 0..IMAGE_WIDTH {
+            eprintln!("Scanlines remaining: {}", j);
+            let r = (i as f32) / ((IMAGE_WIDTH - 1) as f32);
+            let g = (j as f32) / ((IMAGE_HEIGHT - 1) as f32);
+            let b = 0.25f32;
+
+            let r = (r * 255.999f32) as i32;
+            let g = (g * 255.999f32) as i32;
+            let b = (b * 255.999f32) as i32;
+
+            println!("{} {} {}", r, g, b);
+        }
+    }
+    eprintln!("Done!");
 }
